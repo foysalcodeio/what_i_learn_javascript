9369e4d746ee4612ab9a8364b3feb445472597d0
Md. Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2023-04-26 01:25:59 -0700

    Update slide.js

--- slide.js
+++ slide.js
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button onclick="prev()">Previews</button>
+    <img src="./images/ff.jpg" alt="" height="300" width="300">
+
+    <button onclick="next()">Next</button>
+
+</body>
+    <script src="./slide.js"></script>
+
+</html>
+
+-------------- javascript ----------------
+
 var photos = ["images/ff2.jpg", "images/ff.jpg", "images/ff3.jpg"]
 
 var imgTag = document.querySelector("img")
